df809759580e0b6e232c8b8e05b2990a7b2be987
Luis Kugel <luis.kugel@idot-digital.de>
2025-05-15 22:56:07 +0200

    First grpc draft

---
+++ Makefile
@@ -0,0 +1,21 @@
+.PHONY: build clean proto sqlc
+
+# Build the eventsdb binary
+build:
+	go build -o bin/eventsdb eventsdb.go
+
+# Clean build artifacts
+clean:
+	rm -rf bin/
+
+# Create bin directory if it doesn't exist
+bin:
+	mkdir -p bin
+
+# Generate gRPC code from proto file
+proto:
+	protoc --go_out=./grpc --go_opt=paths=source_relative --go-grpc_out=./grpc --go-grpc_opt=paths=source_relative eventsdb.proto
+
+# Generate SQL code using sqlc
+sqlc:
+	sqlc generate
---
+++ query.sql
@@ -0,0 +1,57 @@
+-- name: CreateEvent :execlastid
+INSERT INTO
+  events (`source`, `type`, `subject`, `data`)
+VALUES
+  (?, ?, ?, ?);
+
+-- name: GetEventByID :one
+SELECT
+  *
+FROM
+  events
+WHERE
+  id = ?
+LIMIT ?;
+
+-- name: GetEventsBySubject :many
+SELECT
+  *
+FROM
+  events
+WHERE
+  `id` > ?
+  AND `subject` = ?
+LIMIT
+  ?;
+
+-- name: GetEventsBySubjectPrefix :many
+SELECT
+  *
+FROM
+  events
+WHERE
+  `id` > ?
+  AND MATCH(`subject`) AGAINST (? IN BOOLEAN MODE)
+LIMIT 50;
+
+-- name: GetEventsBySubjectAndType :many
+SELECT
+  *
+FROM
+  events
+WHERE
+  `id` > ?
+  AND `subject` = ?
+  AND `type` = ?
+LIMIT 50;
+
+-- name: GetEventsBySubjectPrefixAndType :many
+SELECT
+  *
+FROM
+  events
+WHERE
+  `id` > ?
+  AND MATCH(`subject`) AGAINST ($2 IN BOOLEAN MODE)
+  AND `type` = $3
+LIMIT 50;
